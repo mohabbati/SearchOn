45eaa8371c8036276f0d355e3eaf6846fcc2d450
mohabbati <mohabbati@live.com>
2022-09-24 19:02:17 +0300

    Add Group search (#12)

---
+++ src/SearchOn.Models/Searches/Group.cs
@@ -0,0 +1,23 @@
+﻿namespace SearchOn.Models.Searches;
+
+public class Group : ISearchModel, IResultModel
+{
+    public Guid Id { get; set; }
+    
+    public string? Name { get; set; }
+    
+    public string? Description { get; set; }
+
+    public decimal Score { get; set; } = 0;
+
+    public int Weight(string propertyName)
+    {
+        return propertyName switch
+        {
+            nameof(Id) => 0,
+            nameof(Name) => 9,
+            nameof(Description) => 5,
+            _ => 0
+        };
+    }
+}
