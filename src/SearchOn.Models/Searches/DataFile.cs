6d2241b181e0cde0e3d59bc4057aee50355a3a88
Mohammad Mohabbati <mohabbati@live.com>
2022-09-24 18:55:29 +0300

    Add DataFile to index data

---
+++ src/SearchOn.Models/Searches/DataFile.cs
@@ -0,0 +1,18 @@
+﻿using Newtonsoft.Json;
+
+namespace SearchOn.Models.Searches;
+
+public class DataFile : ISearchable
+{
+    [JsonProperty("buildings")]
+    public List<Entities.Building> Buildings { get; set; }
+
+    [JsonProperty("locks")]
+    public List<Entities.Lock> Locks { get; set; }
+
+    [JsonProperty("groups")]
+    public List<Entities.Group> Groups { get; set; }
+
+    [JsonProperty("media")]
+    public List<Entities.Medium> Mediums { get; set; }
+}
