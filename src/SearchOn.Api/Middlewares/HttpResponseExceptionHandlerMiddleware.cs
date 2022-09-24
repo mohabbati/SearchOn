cb80c904271588b7fe1b8cdb2b207003b2c65260
Mohammad Mohabbati <mohabbati@live.com>
2022-09-24 19:07:51 +0300

    Configure and refactore Api project

---
+++ src/SearchOn.Api/Middlewares/HttpResponseExceptionHandlerMiddleware.cs
@@ -0,0 +1,62 @@
+﻿using SearchOn.Shared.Exceptions;
+using System.Net;
+using System.Reflection;
+using ILogger = Serilog.ILogger;
+
+namespace SearchOn.Api.Middlewares;
+
+public class HttpResponseExceptionHandlerMiddleware
+{
+    private readonly RequestDelegate _next;
+
+    public HttpResponseExceptionHandlerMiddleware(RequestDelegate next)
+    {
+        _next = next;
+    }
+
+    public async Task Invoke(HttpContext context, IHostEnvironment hostEnvironment, ILogger logger)
+    {
+        context.Response.Headers.Add("Request-ID", context.TraceIdentifier);
+
+        try
+        {
+            await _next(context);
+        }
+        catch (Exception ex)
+        {
+            var exception = UnWrapException(ex);
+
+            if (ex is not LogicException)
+            {
+                logger.Error(exception, "");
+            }
+
+            context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+
+            if (hostEnvironment.IsDevelopment())
+            {
+                await context.Response.WriteAsJsonAsync(exception);
+            }
+            else
+            {
+                await context.Response.WriteAsJsonAsync("An unexpected error occurred.");
+            }
+        }
+
+        Exception UnWrapException(Exception exp)
+        {
+            if (exp is TargetInvocationException)
+                return exp.InnerException ?? exp;
+
+            return exp;
+        }
+    }
+}
+
+public static class HttpResponseExceptionHandlerMiddlewareExtensions
+{
+    public static IApplicationBuilder UseHttpResponseExceptionHandler(this IApplicationBuilder builder)
+    {
+        return builder.UseMiddleware<HttpResponseExceptionHandlerMiddleware>();
+    }
+}
--- src/SearchOn.Api/Program.cs
+++ src/SearchOn.Api/Program.cs
@@ -1,25 +1,24 @@
-var builder = WebApplication.CreateBuilder(args);
+using Serilog;
 
-// Add services to the container.
-
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+try
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    var builder = WebApplication.CreateBuilder(args);
+
+    builder.Host.ConfigureLogger(builder.Environment);
+
+    SearchOn.Api.Startup.Services.Add(builder.Services, builder.Configuration);
+
+    var app = builder.Build();
+
+    SearchOn.Api.Startup.Middlewares.Use(app, builder.Environment);
+
+    app.Run();
 }
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
+catch (Exception ex)
+{
+    Log.Fatal(ex, "An unhandled exception occurred.");
+}
+finally
+{
+    Log.CloseAndFlush();
+}
