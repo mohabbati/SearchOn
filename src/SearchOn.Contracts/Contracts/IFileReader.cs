0972b6b7ab67804314c873ff143405b131d8556b
Mohammad Mohabbati <mohabbati@live.com>
2022-09-24 17:43:36 +0300

    Add JSON file reader

---
+++ src/SearchOn.Contracts/Contracts/IFileReader.cs
@@ -0,0 +1,8 @@
+﻿namespace SearchOn.Contracts;
+
+public interface IFileReader<TModel> where TModel : class, new()
+{
+    Task<TModel> ReadFileAsync(string filePath, CancellationToken cancellationToken);
+
+    Task<TModel> ReadFileAsync(CancellationToken cancellationToken);
+}
---
+++ src/SearchOn.Services/Implementations/FileReader.cs
@@ -0,0 +1,25 @@
+﻿using Newtonsoft.Json;
+
+namespace SearchOn.Services.Implementations;
+
+public class FileReader<TModel> : IFileReader<TModel>
+    where TModel: class, ISearchable, new()
+{
+    public async Task<TModel> ReadFileAsync(CancellationToken cancellationToken)
+    {
+        return await ReadFileAsync(@"d:\temp\sv_lsm_data.json", cancellationToken);
+    }
+
+    public async Task<TModel> ReadFileAsync(string filePath, CancellationToken cancellationToken)
+    {
+        using StreamReader file = File.OpenText(filePath);
+
+        var json = await file.ReadToEndAsync();
+        var dataFile = JsonConvert.DeserializeObject<TModel>(json);
+
+        if (dataFile is null)
+            throw new Exception("There is no data.");
+
+        return dataFile;
+    }
+}
