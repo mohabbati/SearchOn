7ab4596f815156dfaa10335e17c062af8ed7b3d0
mohabbati <mohabbati@live.com>
2022-09-24 18:06:32 +0300

    Add search contracts (#6)

---
+++ src/SearchOn.Contracts/Contracts/IResultModel.cs
@@ -0,0 +1,8 @@
+﻿namespace SearchOn.Contracts;
+
+public interface IResultModel
+{
+    Guid Id { get; set; }
+
+    decimal Score { get; set; }
+}
---
+++ src/SearchOn.Contracts/Contracts/ISearch.cs
@@ -0,0 +1,10 @@
+﻿namespace SearchOn.Contracts;
+
+public interface ISearch<TSearchModel, TResultModel>
+    where TSearchModel : class, ISearchModel, new()
+    where TResultModel : class, IResultModel, new()
+{
+    IEnumerable<TSearchModel> Index(ISearchable document);
+
+    IEnumerable<TResultModel> Search(IEnumerable<TSearchModel> index, string criteria);
+}
